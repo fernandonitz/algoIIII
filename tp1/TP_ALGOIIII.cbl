668357779238f5723f56b7f872eff4f4c3de293b
fernandonitz <nitzfernando@gmail.com>
2016-10-21 17:23:14 -0300

    se agrega un comando para cambiar rapidamente las rutas, los archivos de pruebas y cambios en el tp

--- tp1/TP_ALGOIIII.cbl
+++ tp1/TP_ALGOIIII.cbl
@@ -13,66 +13,72 @@ FILE-CONTROL.
 	SELECT SUC3 ASSIGN TO DISK ORGANIZATION IS LINE SEQUENTIAL FILE STATUS IS suc3-estado.
 	SELECT TIM ASSIGN TO DISK ORGANIZATION IS LINE SEQUENTIAL FILE STATUS IS times-estado.
 	SELECT SUCURSALES ASSIGN TO DISK ORGANIZATION IS LINE SEQUENTIAL FILE STATUS IS sucursales-estado.
-	SELECT  TIPOSCLASE ASSIGN TO DISK ORGANIZATION IS LINE SEQUENTIAL FILE STATUS IS tipos_clase-estado.
+	SELECT TIPOSCLASE ASSIGN TO DISK ORGANIZATION IS LINE SEQUENTIAL FILE STATUS IS tipos_clase-estado.
     SELECT MASTER ASSIGN TO DISK ORGANIZATION IS LINE SEQUENTIAL FILE STATUS IS mae-estado.
 DATA DIVISION.
 FILE SECTION.
 
 FD SUC1 LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/suc1.txt".
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/suc1.txt".
 01 reg_suc1.
-	  03 suc_1_num PIC X(5).
-	  03 suc_1_fecha PIC 9(8).
-	  03 suc_1_suc PIC X(3).
-	  03 suc_1_clase PIC X(4).
-	  03 suc_1_horas PIC 9(2)V99.
+	  03 suc_1_num 						PIC X(5).
+	  03 suc_1_fecha 					PIC 9(8).
+	  03 suc_1_suc 						PIC X(3).
+	  03 suc_1_clase 					PIC X(4).
+	  03 suc_1_horas 					PIC 9(2)V99.
 	  
 FD SUC2 LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/suc2.txt".
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/suc2.txt".
 01 reg_suc2.
-	  03 suc_2_num PIC X(5).
-	  03 suc_2_fecha PIC 9(8).
-	  03 suc_2_suc PIC X(3).
-	  03 suc_2_clase PIC X(4).
-	  03 suc_2_horas PIC 9(2)V99.
+	  03 suc_2_num 						PIC X(5).
+	  03 suc_2_fecha 					PIC 9(8).
+	  03 suc_2_suc 						PIC X(3).
+	  03 suc_2_clase 					PIC X(4).
+	  03 suc_2_horas 					PIC 9(2)V99.
 	  
 FD SUC2 LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/suc3.txt".
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/suc3.txt".
 01 reg_suc3.
-	  03 suc_3_num PIC X(5).
-	  03 suc_3_fecha PIC 9(8).
-	  03 suc_3_suc PIC X(3).
-	  03 suc_3_clase PIC X(4).
-	  03 suc_3_horas PIC 9(2)V99.
+	  03 suc_3_num 						PIC X(5).
+	  03 suc_3_fecha 					PIC 9(8).
+	  03 suc_3_suc 						PIC X(3).
+	  03 suc_3_clase 					PIC X(4).
+	  03 suc_3_horas 					PIC 9(2)V99.
 	  
 FD TIM LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/times.txt".
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/times.txt".
 01 reg_time.
-	  03 tim_num PIC X(5).
-	  03 tim_fecha PIC 9(8).
-	  03 tim_suc PIC X(3).
-	  03 tim_clase PIC X(4).
-	  03 tim_horas PIC 9(2).
+	  03 tim_num 						PIC X(5).
+	  03 tim_fecha 						PIC 9(8).
+	  03 tim_suc 						PIC X(3).
+	  03 tim_clase 						PIC X(4).
+	  03 tim_horas 						PIC 9(2)V99.
 
 FD SUCURSALES LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/sucursales.txt".
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/sucursales.txt".
 01 reg_sucursal.
-	  03 suc_suc PIC X(3).
-	  03 suc_razon PIC 9(25).
-	  03 suc_dire PIC X(20).
-	  03 suc_tel PIC X(20).
-	  03 suc_cuit PIC 9(11).
+	  03 suc_suc 						PIC X(3).
+	  03 suc_razon 						PIC 9(25).
+	  03 suc_dire 						PIC X(20).
+	  03 suc_tel 						PIC X(20).
+	  03 suc_cuit 						PIC 9(11).
 
 FD TIPOSCLASE LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/tiposclase.txt".
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/tiposclase.txt".
 01 reg_tipclase.
-	  03 tip_clase_suc PIC X(4).
-	  03 tip_clase_razon PIC X(20).
-	  03 tip_clase_dire PIC 9(5)V99.
+	  03 tip_clase_suc 					PIC X(4).
+	  03 tip_clase_razon 				PIC X(20).
+	  03 tip_clase_dire 				PIC 9(5)V99.
 	  
 FD MASTER LABEL RECORD IS STANDARD 
-		VALUE OF FILE-ID IS "/home/franguerini/Desktop/tp/algoiiii/tp1/archivos/mae.txt".
-01 reg_mae PIC X(1).
+		VALUE OF FILE-ID IS "/home/fernando/workspaces/workspace/algo4/algoIIII/tp1/archivos/mae.txt".
+01 reg_mae 								PIC X(5).
+*>01 reg_mae.
+*>	  03 mae_num PIC X(5).
+*>	  03 mae_fecha PIC X(8).
+*>	  03 mae_suc PIC X(3).
+*>	  03 mae_clase PIC X(4).
+*>	  03 mae_horas PIC X(4).
 
 WORKING-STORAGE SECTION.
 
@@ -100,10 +106,15 @@ WORKING-STORAGE SECTION.
 	77 WB-FIN-ENTRADA 					PIC X(1) VALUE "N".
     	88 FIN-ENTRADA 					VALUE "S".
 
+   	*> TOTALES PARCIALES:
 	01 TOT_GRAL 						PIC 9(6) VALUE 0.
 	01 TOT_X_PROF 						PIC 9(6) VALUE 0.
 	01 TOT_X_FECHA 						PIC 9(6) VALUE 0.
+	
+	*> ITERADOR
 	01 I 								PIC 9(1) VALUE 0.
+	
+	*> FIN DE ARCHIVOS:
 	01 suc1-estado 						PIC XX.
 	01 suc2-estado 						PIC XX.
 	01 suc3-estado 						PIC XX.
@@ -111,6 +122,10 @@ WORKING-STORAGE SECTION.
 	01 sucursales-estado 				PIC XX.
 	01 tipos_clase-estado 				PIC XX.
 	01 mae-estado						PIC XX.
+
+	*> REGISTROS MINIMOS:
+	01 prof_min							PIC X(5).
+
 PROCEDURE DIVISION.
 
 *> CICLO PPAL--------------------------------------------------------------
@@ -120,7 +135,6 @@ PROCEDURE DIVISION.
 	PERFORM 3_ARMO_V_SUCURSALES.
 	PERFORM 4_ARMO_V_TIPOS_CLASE.
 	PERFORM 5_CICLO_ARCHIVOS UNTIL suc1_estado_eof.
-*> corte cuando eof de todos los archivos: EOF suc1 and EOF suc2 and EOF suc3 and EOF times
 	PERFORM 6_IMPRIMO_MATRIZ.
 	PERFORM 7_CIERRO_ARCHIVOS.
 	STOP RUN.   
@@ -139,8 +153,9 @@ PROCEDURE DIVISION.
 
 2_LEO_ARCHIVOS.
 	READ SUC1 AT END MOVE "SI" TO suc1-estado.
-
-*> primera lectura de los archivos suc1, suc2, suc3 y times y guardado en un registo particular de cada uno ej: reg_suc1 = Leo(suc1)
+	READ SUC2 AT END MOVE "SI" TO suc2-estado.
+	READ SUC3 AT END MOVE "SI" TO suc3-estado.
+	READ TIM  AT END MOVE "SI" TO times-estado.
 
 3_ARMO_V_SUCURSALES.
 *> se trae a memoria el archivo de sucursales con su respectivo formato y de ser necesario su indice
@@ -151,8 +166,10 @@ PROCEDURE DIVISION.
 5_CICLO_ARCHIVOS.
 	PERFORM 51_OBTENER_REG_MIN_PROF.
 	MOVE 0 TO TOT_X_PROF.
+	
 	PERFORM 52_CICLO_PROFESORES UNTIL suc1_estado_eof.
 *> corte cuando eof de todos los archivos: EOF suc1 and EOF suc2 and EOF suc3 and EOF times and Prof_ant != Prof_act
+	
 	PERFORM 53_ESCRIBO_TOT_PROF.
 	PERFORM 54_IMPRIMO_TOT_PROF.
 
@@ -171,6 +188,8 @@ PROCEDURE DIVISION.
 *> CICLO POR PROFESOR--------------------------------------------------------------
 
 51_OBTENER_REG_MIN_PROF.
+
+
 *> se debe: inicializar el reg_min con el minimo de entre: reg_suc1, reg_suc2, reg_suc3, reg_times (comparando por numero de profesor) ya que se debe inicializar prof_anterior
 
 52_CICLO_PROFESORES.
@@ -222,8 +241,16 @@ PROCEDURE DIVISION.
 	DISPLAY suc_1_suc.
 	DISPLAY suc_1_clase.
 	DISPLAY suc_1_horas.
+*>	MOVE CORRESPONDING reg_suc1 TO reg_mae.
+	MOVE "HOLAS" TO reg_mae.
+	WRITE reg_mae.
+
 *> se debe escribir el movimiento individual como lo dice el enunciado
 
 5224_LEO_ARCH_MIN.
 	READ suc1 AT END MOVE "SI" TO suc1_estado.
+	READ SUC2 AT END MOVE "SI" TO suc2-estado.
+	READ SUC3 AT END MOVE "SI" TO suc3-estado.
+	READ TIM  AT END MOVE "SI" TO times-estado.
+
 *>  ya teniendo el archivo min, se debe dejar el resultado en el nuevo registro.
